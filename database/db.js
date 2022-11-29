87f0c05c385bfbadf8c4600bca4c46cb153a9c4b
Tanzir05 <110198760+Tanzir05@users.noreply.github.com>
2022-11-29 11:25:18 +0600

    Add files via upload

---
+++ database/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //db: 'mongodb://localhost:27017/reactdb'
+  db: 'mongodb://mernsms2:kYSRtynW3x4BP52ahDcHR4C5vjjkEVXfIX5b1CHsLFLI32GqH0bP0Qn5rb69WmUzDbwhaaogiObtACDbs3UvUw==@mernsms2.mongo.cosmos.azure.com:10255/mern-sms2?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@mernsms2@'
+};
